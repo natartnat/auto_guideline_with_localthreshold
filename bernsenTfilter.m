a5d220cf3c58bc3f28b4abb27d27d4e3658b0d1b
Nat anuntalabhochai <natartnat@gmail.com>
2016-09-01 00:02:30 +0700

    Delete bernsenTfilter.m

--- bernsenTfilter.m
+++
@@ -1,4 +0,0 @@
-function [localMaxImage,localMinImage] = bernsenTfilter(inputImage,winLength)
-localMaxImage = colfilt(inputImage, [winLength winLength], 'sliding', @max);
-localMinImage = colfilt(inputImage, [winLength winLength], 'sliding', @min);
-end
